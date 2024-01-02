cd2bc4114e36625e622bb3dde34ac972e42dcf61
onokajames96 <jamesonoka996@gmail.com>
2024-01-02 03:24:57 -0800

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""returns information about his/her TODO list progress.."""
+"""returns employees information."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
     url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
-
-    completed_tasks = [task["title"] for task in todos if task["completed"]]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user["name"], len(completed_tasks), len(todos)))
-
-    [print("\t {}".format(task)) for task in completed_tasks]
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
